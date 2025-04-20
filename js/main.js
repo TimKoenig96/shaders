7a7da6784a2c2c9bcba3184941d1101c7c4fb74d
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-04-20 19:50:10 +0200

    add navbar setup function

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const SHADERS = new Map([
+	["water-1", {
+		label: "Water 1",
+		desc: "First ever attempt at making shaders.",
+		date: "20th Apr 2025"
+	}]
+]);
+
+function setupNavbar() {
+	const navbar = document.getElementById("navbar");
+
+	for (const [identifier, { label, desc, date }] of SHADERS) {
+		const button = document.createElement("div");
+		button.classList.add("button");
+
+		button.innerHTML = `
+			<p class="label">${label ?? "MISSING LABEL: " + identifier}</p><hr>
+			<p class="desc">${desc ?? "MISSING DESC: " + identifier}</p><hr>
+			<p class="date">${date ?? "MISSING DATE: " + identifier}</p>
+		`;
+
+		button.dataset.shader = identifier;
+		navbar.appendChild(button);
+	}
+}
+
+function init() {
+	setupNavbar();
+}
+
+init();
