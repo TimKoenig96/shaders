155468a3f80ddbef144ef711581ae23e4e62a0df
Tim König <137522127+TimKoenig96@users.noreply.github.com>
2025-04-25 19:20:18 +0200

    add fullscreen triangle geometry

---
+++ shaders/shared/geometry/fullscreenTriangle.js
@@ -0,0 +1,11 @@
+export default {
+	drawMode: WebGL2RenderingContext.TRIANGLES,
+	twoDimensional: true,
+	attributes: {
+		a_position: new Float32Array([
+			-1.0,  1.0,
+			 3.0,  1.0,
+			-1.0, -3.0
+		])
+	}
+};
